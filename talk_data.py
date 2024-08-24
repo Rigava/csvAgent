5995fb3651f1ed567b76c33e0689921c16d07c40
NetZero <73232007+Rigava@users.noreply.github.com>
2024-08-24 13:28:42 +0530

    Update talk_data.py

--- talk_data.py
+++ talk_data.py
@@ -18,7 +18,7 @@ google_api_key=key,
 convert_system_message_to_human=True
 )
 
-st.set_page_config(page_title="👨‍💻 Talk with your Data")
+st.set_page_config(page_title="👨‍💻 Talk with your Data",layout="wide")
 st.title("👨‍💻 Talk with your Data")
 st.write("Please upload your CSV/XLS file below.")
 
